0a31532472469c92b06b4782474053f2d20f5c70
Riddhesh Sanghvi <riddhesh237@gmail.com>
2020-08-04 11:43:06 +0530

    Add cleanup of home as well as event
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,15 @@ set -e # fail on error
 # include hidden files 
 # https://askubuntu.com/questions/740805/how-can-i-remove-all-files-from-current-directory-using-terminal
 shopt -s dotglob
+echo "Cleaning up Workspace directory."
 rm -rf *
 
+# Cleanup home directory
+echo "Cleaning up home directory."
+[[ -d "$HOME" ]] && cd "$HOME" && rm -rf *
+
+# Cleanup event json
+echo "Cleaning up event.json."
+[[ -f "$GITHUB_EVENT_PATH" ]] && rm $GITHUB_EVENT_PATH
+
+echo "Post job cleanup complete."
